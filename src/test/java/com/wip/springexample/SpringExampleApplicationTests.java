607263ffe743dd755ee0937813588a08b13f798a
Arlan Galvez Alonso <galvezalonso@gmail.com>
2023-02-02 12:37:15 -0500

    feat: base project config

---
+++ src/test/java/com/wip/springexample/SpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wip.springexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
