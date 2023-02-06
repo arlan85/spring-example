6c483c6390ec5f54d25d93b43ccd0e7af7b3a3b3
Arlan Galvez Alonso <galvezalonso@gmail.com>
2023-02-06 10:45:16 -0500

    Add creat and update actions

--- src/main/java/com/wip/springexample/controller/CustomerController.java
+++ src/main/java/com/wip/springexample/controller/CustomerController.java
@@ -1,10 +1,10 @@
 package com.wip.springexample.controller;
 
+import com.wip.springexample.dto.CustomerDTO;
 import com.wip.springexample.entities.Customer;
 import com.wip.springexample.services.CustomerService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +20,16 @@ public class CustomerController {
     @GetMapping()
     public List<Customer> getCustomers(){
         return this.customerService.getAllCustomers();
+    }
+
+    @PostMapping()
+    public ResponseEntity<Void> addCustomer(@RequestBody CustomerDTO customerDTO) {
+        this.customerService.addCustomer(customerDTO);
+        return ResponseEntity.noContent().build();
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable("id") Integer id) {
+        this.customerService.deleteCustomer(id);
+        return  ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/wip/springexample/services/CustomerService.java
+++ src/main/java/com/wip/springexample/services/CustomerService.java
@@ -1,6 +1,8 @@
 package com.wip.springexample.services;
 
 import com.wip.springexample.Repositories.CustomerRepository;
+import com.wip.springexample.dto.CustomerDTO;
+import com.wip.springexample.dto.mapper.MapDTOToCustomer;
 import com.wip.springexample.entities.Customer;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +11,23 @@ import java.util.List;
 @Service
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final MapDTOToCustomer mapper;
 
-    public CustomerService(CustomerRepository customerRepository) {
+    public CustomerService(CustomerRepository customerRepository, MapDTOToCustomer mapper) {
         this.customerRepository = customerRepository;
+        this.mapper = mapper;
     }
 
     public List<Customer> getAllCustomers(){
         return this.customerRepository.findAll();
+    }
+
+    public void addCustomer(CustomerDTO customerDTO) {
+        Customer customer = this.mapper.map(customerDTO);
+        this.customerRepository.save(customer);
+    }
+
+    public void deleteCustomer(Integer id){
+        this.customerRepository.deleteById(id);
     }
 }
