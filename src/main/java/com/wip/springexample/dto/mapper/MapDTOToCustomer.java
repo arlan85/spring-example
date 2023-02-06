1a27feff902dd02d9970e30fa0f58933d655bc3a
Arlan Galvez Alonso <galvezalonso@gmail.com>
2023-02-06 10:44:53 -0500

    Update DTO to be injected as service

--- src/main/java/com/wip/springexample/dto/mapper/MapDTOToCustomer.java
+++ src/main/java/com/wip/springexample/dto/mapper/MapDTOToCustomer.java
@@ -2,7 +2,9 @@ package com.wip.springexample.dto.mapper;
 
 import com.wip.springexample.dto.CustomerDTO;
 import com.wip.springexample.entities.Customer;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MapDTOToCustomer implements IMapper<CustomerDTO, Customer> {
     @Override
     public Customer map(CustomerDTO in) {
